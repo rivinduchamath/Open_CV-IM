79f71bb323753bfd040fd44aaaf6b7822f7ea3ce
Wijayarathna P.G.R.C <it18141948@my.sliit.lk>
2022-03-23 11:21:44 +0530

    a

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import cv2
 
-img = cv2.imread("C:/Users/wogza/Pictures/aa.jpg",1)
+img = cv2.imread("C:/Users/wogza/Pictures/aa.jpg", 1)
 cv2.imshow("Input Window", img)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+
+# Change Colour Space
+flags = [i for i in dir(cv2) if i.startswith('COLOR_')]
+print(flags)
+
+# convert to gray
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray Image", imgGray)
+
+# convert to HSV
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+cv2.imshow("HSV Image", imgGray)
+
 cv2.waitKey()
 cv2.destroyAllWindows()
 
-
+vid = cv2.VideoCapture("PATH")
+# while(vid.isOpened()):
