de0d14a33b85444115386645ca5638bb20ea5024
Wijayarathna P.G.R.C <it18141948@my.sliit.lk>
2022-03-23 15:31:40 +0530

    a

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 img = cv2.imread("C:/Users/wogza/Pictures/aa.jpg", 1)
 cv2.imshow("Input Window", img)
@@ -20,12 +21,44 @@ cv2.imshow("HSV Image", imgGray)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
+# *************************************************************
+# Lab 2
+# *************************************************************
+
+# Load or read the video
 vid = cv2.VideoCapture("C:/Users/wogza/Downloads/q.mp4")
+
+# Take each Frame of the video
 while vid.isOpened():
     ret, frame = vid.read()
 
-    originalVideo = cv2.resize(frame,(400, 400))
+    # Converting BGR to HSV color-space
+    hsvImg = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Define lower rang and upper range of blue
+    lower_Blue = np.array([110, 50, 50])
+    upper_Blue = np.array([130, 255, 255])
+
+    # There should be HSV image for a range of blue colour
+    blueMusk = cv2.inRange(hsvImg, lower_Blue, upper_Blue)
+
+# Extract the blue object alone
+    result = cv2.bitwise_and(frame, frame, mask=blueMusk)
+
+    # Display Original Video
+    originalVideo = cv2.resize(frame, (400, 400))
     cv2.imshow("Original Video", originalVideo)
+
+    # Display Binary Video
+    binaryVideo = cv2.resize(blueMusk, (400, 400))
+    cv2.imshow("Binary Video", binaryVideo)
+
+    # Display masked / object Video
+    maskedVideo = cv2.resize(result, (400, 400))
+    cv2.imshow("Masked Video", maskedVideo)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+vid.release()
+cv2.destroyAllWindows()
